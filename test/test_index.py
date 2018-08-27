0ffe59b335ea71982292c0c3a9f60a17df9760be
tkoar <terrancekoar@gmail.com>
2018-08-27 17:16:43 -0400

    added tests and solution

--- test/test_index.py
+++ test/test_index.py
@@ -1,30 +1,37 @@
 import pytest
-from ipynb.fs.full.index import cities, city_indices, city_names, names_and_ranks, city_populations, city_areas
+from ipynb.fs.full.index import first_dog_person, iteration_count, cat_owners, thirty_something_yr_old, dog_owner_names, dog_names, cat_owner_names, cat_names, list_of_odd_numbers_plus_ten
 
-def test_cities_list():
-    assert type(cities) == type([])
-    assert len(cities) == 12
+def test_find_first_dog_person():
+    assert first_dog_person == {'name': "Katie", 'age': 30, 'job': "Teacher", 'pet': "Dog", 'pet_name': "Frank"}
+    assert iteration_count > 0
+    assert iteration_count <= 2
 
-def test_city_indices():
-    assert type(city_indices) == type([])
-    assert city_indices == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+def test_cat_owners():
+    assert type(cat_owners) == type([])
+    assert len(cat_owners) == 2
+    assert cat_owners == [{'name': 'Owen', 'age': 26, 'job': 'Sales person', 'pet': 'Cat', 'pet_name': 'Cosmo'}, {'name': 'Josh', 'age': 22, 'job': 'Student', 'pet': 'Cat', 'pet_name': 'Chat'}]
 
-def test_city_names():
-    assert type(city_names) == type([])
-    assert len(city_names) == 12
-    assert city_names == ['Buenos Aires', 'Toronto', 'Pyeongchang', 'Marakesh', 'Albuquerque', 'Los Cabos', 'Greenville', 'Archipelago Sea', 'Walla Walla Valley', 'Salina Island', 'Solta', 'Iguazu Falls']
+def test_thirty_something_yr_old():
+    assert type(thirty_something_yr_old) == type({})
+    assert thirty_something_yr_old == {'name': "Katie", 'age': 30, 'job': "Teacher", 'pet': "Dog", 'pet_name': "Frank"}
 
-def test_names_ranks():
-    assert type(names_and_ranks) == type([])
-    assert len(names_and_ranks) == 12
-    assert names_and_ranks == ['1. Buenos Aires', '2. Toronto', '3. Pyeongchang', '4. Marakesh', '5. Albuquerque', '6. Los Cabos', '7. Greenville', '8. Archipelago Sea', '9. Walla Walla Valley', '10. Salina Island', '11. Solta', '12. Iguazu Falls']
+def test_dog_owner_names():
+    assert type(dog_owner_names) == type([])
+    assert dog_owner_names == ['Katie', 'Estelle', 'Gustav']
 
-def test_city_populations():
-    assert type(city_populations) == type([])
-    assert len(city_populations) == 12
-    assert city_populations == [2891000, 2800000, 2581000, 928850, 559277, 287651, 84554, 60000, 32237, 4000, 1700, 0]
+def test_dog_names():
+    assert type(dog_names) == type([])
+    assert dog_names == ['Frank', 'Gabby', 'Helen']
 
-def test_city_areas():
-    assert type(city_areas) == type([])
-    assert len(city_areas) == 12
-    assert city_areas == [4758, 2731, 3194, 200, 491, 3750, 68, 8300, 33, 27, 59, 672]
+def test_cat_owner_names():
+    assert type(cat_owner_names) == type([])
+    assert cat_owner_names == ['Daniel', 'Owen', 'Josh']
+
+def test_cat_names():
+    assert type(cat_names) == type([])
+    assert cat_names == ['Gato', 'Cosmo', 'Chat']
+
+def test_list_of_odd_numbers_plus_ten():
+    assert type(list_of_odd_numbers_plus_ten) == type([])
+    assert len(list_of_odd_numbers_plus_ten) == 35
+    assert list_of_odd_numbers_plus_ten == [11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79]
